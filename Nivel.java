0a1240317a75402ce2312ed43b52351ff4df7cf6
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-10-07 21:37:24 -0300

    agr sim parte 2

--- Nivel.java
+++ Nivel.java
@@ -23,7 +23,7 @@ public class Nivel
 
 	public int DeterminarNivel()
 	{
-		if(intensidadeExercicio == 0 && exercicios = 0 && junkFood > 10) // nível 1
+		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood > 10) // nível 1
 			return 1;
 		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood > 10) //nível 2
 			return 2;
@@ -33,19 +33,19 @@ public class Nivel
 			return 2;
 		if(intensidadeExercicio == 2 && exercicios >=4 && junkFood > 10)
 			return 2;
-		if(intensidadeExercicio == 0 && exercicios = 0 && junkFood >= 5)
+		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood >= 5)
 			return 3;
 		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood >= 5)
 			return 3;
 		if(intensidadeExercicio == 2 && (exercicios == 2 || exercicios == 3) && junkFood >= 5)
 			return 3;
-		if(intensidadeExercicio == 0 && exercicios = 0 && junkFood < 5)
+		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood < 5)
 			return 4;
 		if(intensidadeExercicio == 1 && exercicios >=4 && junkFood >=5)
 			return 4;
 		if(intensidadeExercicio == 2 && exercicios >=4 && junkFood >=5)
 			return 4;
-		if(intensidadeExercicio == 0 && exercicios = 0 && junkFood <=1)
+		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood <=1)
 			return 5;
 		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood < 5)
 			return 5;
