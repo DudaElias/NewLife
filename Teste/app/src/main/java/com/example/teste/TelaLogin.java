a55a2e22980f24c3c71c4bc35755632e464b75ff
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-10-11 13:54:51 -0300

    safa

--- Teste/app/src/main/java/com/example/teste/TelaLogin.java
+++ Teste/app/src/main/java/com/example/teste/TelaLogin.java
@@ -2,8 +2,11 @@ package com.example.teste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -16,6 +19,7 @@ public class TelaLogin extends AppCompatActivity {
 
     ViewFlipper flipper;
     TextView login, criar;
+    private Animation slide_in_left, slide_in_right, slide_out_left, slide_out_right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,11 +28,17 @@ public class TelaLogin extends AppCompatActivity {
         login = findViewById(R.id.txtLogin);
         criar = findViewById(R.id.txtCriar);
 
+        slide_in_left = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
+        slide_in_right = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
+        slide_out_left = AnimationUtils.loadAnimation(this, R.anim.slide_out_left);
+        slide_out_right = AnimationUtils.loadAnimation(this, R.anim.slide_out_right);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(flipper.getDisplayedChild() == 0)
                     return;
+                flipper.setInAnimation(slide_in_left);
+                flipper.setOutAnimation(slide_out_right);
                 flipper.showPrevious();
             }
         });
@@ -38,6 +48,8 @@ public class TelaLogin extends AppCompatActivity {
             public void onClick(View view) {
                 if(flipper.getDisplayedChild() == 1)
                     return;
+                flipper.setInAnimation(slide_in_right);
+                flipper.setOutAnimation(slide_out_left);
                 flipper.showNext();
             }
         });
