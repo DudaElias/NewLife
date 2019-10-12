a6f414e07bcda88b88b9d883dd68f9e0f32bbf47
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-10-11 22:44:36 -0300

    a

--- Teste/app/src/main/java/com/example/teste/TelaLogin.java
+++ Teste/app/src/main/java/com/example/teste/TelaLogin.java
@@ -2,11 +2,13 @@ package com.example.teste;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -19,6 +21,7 @@ public class TelaLogin extends AppCompatActivity {
 
     ViewFlipper flipper;
     TextView login, criar;
+    Button logar, cadastrar;
     private Animation slide_in_left, slide_in_right, slide_out_left, slide_out_right;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class TelaLogin extends AppCompatActivity {
         flipper = findViewById(R.id.flipper);
         login = findViewById(R.id.txtLogin);
         criar = findViewById(R.id.txtCriar);
+        logar = findViewById(R.id.btnLogar);
+        cadastrar = findViewById(R.id.btnCadastrar);
 
         slide_in_left = AnimationUtils.loadAnimation(this, R.anim.slide_in_left);
         slide_in_right = AnimationUtils.loadAnimation(this, R.anim.slide_in_right);
@@ -51,6 +56,13 @@ public class TelaLogin extends AppCompatActivity {
                 flipper.setInAnimation(slide_in_right);
                 flipper.setOutAnimation(slide_out_left);
                 flipper.showNext();
+            }
+        });
+        logar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent data = new Intent(TelaLogin.this, QuizBase.class);
+                startActivity(data);
             }
         });
     }
