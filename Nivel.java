b48c4f1bc3092148b1815dc8aa968faca197b701
DudaElias <42540760+DudaElias@users.noreply.github.com>
2019-10-13 19:19:58 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAA

--- Nivel.java
+++ Nivel.java
@@ -23,45 +23,29 @@ public class Nivel
 
 	public int DeterminarNivel()
 	{
-		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood > 10) // nível 1
+		if(exercicios == 0 && junkFood > 10) // nível 1
 			return 1;
-		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood > 10) //nível 2
+		if((exercicios == 2 || exercicios == 3) && junkFood > 10) //nível 2
 			return 2;
-		if(intensidadeExercicio == 1 && exercicios >=4 && junkFood > 10)
+		if(exercicios >=4 && junkFood > 10)
 			return 2;
-		if(intensidadeExercicio == 2 && (exercicios == 2 || exercicios == 3) && junkFood > 10)
-			return 2;
-		if(intensidadeExercicio == 2 && exercicios >=4 && junkFood > 10)
-			return 2;
-		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood >= 5) //nível 3
+		if(exercicios == 0 && junkFood >= 5) //nível 3
 			return 3;
-		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood >= 5)
-			return 3;
-		if(intensidadeExercicio == 2 && (exercicios == 2 || exercicios == 3) && junkFood >= 5)
-			return 3;
-		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood < 5) //nível 4
+		if((exercicios == 2 || exercicios == 3) && junkFood >= 5)//nível 4
 			return 4;
-		if(intensidadeExercicio == 1 && exercicios >=4 && junkFood >=5)
-			return 4;
-		if(intensidadeExercicio == 2 && exercicios >=4 && junkFood >=5)
-			return 4;
-		if(intensidadeExercicio == 0 && exercicios == 0 && junkFood <=1) //nível 5
+		if(exercicios == 0 && junkFood < 5) //nível 5
 			return 5;
-		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood < 5)
+		if(exercicios >=4 && junkFood >=5)
 			return 5;
-		if(intensidadeExercicio == 1 && exercicios >=4 && junkFood < 5) //nível 6
+		if(exercicios == 0 && junkFood <=1) //nível 6
 			return 6;
-		if(intensidadeExercicio == 2 && (exercicios == 2 || exercicios == 3) && junkFood < 5) // nível 7
+		if((exercicios == 2 || exercicios == 3) && junkFood < 5)//nivel 7
 			return 7;
-		if(intensidadeExercicio == 2 && exercicios >=4 && junkFood < 5) //nível 8
+		if(exercicios >=4 && junkFood < 5) //nível 8
 			return 8;
-		if(intensidadeExercicio == 1 && (exercicios == 2 || exercicios == 3) && junkFood <=1)//nível 9
+		if((exercicios == 2 || exercicios == 3) && junkFood <=1)//nível 9
 			return 9;
-		if(intensidadeExercicio == 2 && (exercicios == 2 || exercicios == 3) && junkFood <=1)
-			return 9;
-		if(intensidadeExercicio == 1 && exercicios >=4 && junkFood <=1) //nível 10
-			return 10;
-		if(intensidadeExercicio == 2 && exercicios >=4 && junkFood <=1)
+		if(exercicios >=4 && junkFood <=1) //nível 10
 			return 10;
 	}
 }
